728f219a4bc97c911a815b7d32b08cd6e1e0cb8e
Napoleon Oikonomou <Napoleonoikon@gmail.com>
2019-09-21 18:06:01 +0300

    Update SDK, update deps

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { StyleSheet, View, Image, Dimensions, StatusBar } from 'react-native';
-import { Accelerometer } from 'expo-sensors';
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
+import React from "react";
+import { StyleSheet, View, Image, Dimensions, StatusBar } from "react-native";
+import { Accelerometer } from "expo-sensors";
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
 
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 
 export default () => {
   const [tilt, setTilt] = React.useState(0);
@@ -20,14 +20,14 @@ export default () => {
   return loading
     ? (
       <AppLoading
-        startAsync={() => Asset.loadAsync([require('./assets/road.gif'), require('./assets/car.png')])}
+        startAsync={() => Asset.loadAsync([require("./assets/road.gif"), require("./assets/car.png")])}
         onFinish={() => setLoading(false)}
       />
     )
     : (
       <View style={styles.container}>
-        <Image source={require('./assets/road.gif')} style={styles.road} />
-        <Image source={require('./assets/car.png')} style={[styles.car, { left: width * (0.48 + tilt) }]} />
+        <Image source={require("./assets/road.gif")} style={styles.road} />
+        <Image source={require("./assets/car.png")} style={[styles.car, { left: width * (0.48 + tilt) }]} />
       </View>
     );
 };
@@ -35,8 +35,8 @@ export default () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   road: {
     width,
@@ -45,8 +45,8 @@ const styles = StyleSheet.create({
   car: {
     height: 70,
     width: 70,
-    position: 'absolute',
-    resizeMode: 'contain',
+    position: "absolute",
+    resizeMode: "contain",
     bottom: 0,
   },
 });
--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = (api) => {
   api.cache(true);
-  return { presets: ['babel-preset-expo'] };
+  return { presets: ["babel-preset-expo"] };
 };
