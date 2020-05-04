62b0f5c5415e1273231cded11bf23d4094791bc7
Napoleon Oikonomou <Napoleonoikon@gmail.com>
2020-05-04 17:12:13 +0300

    Use hook for accelerometer

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import React from "react";
 import { StyleSheet, View, Image, Dimensions, StatusBar } from "react-native";
-import { Accelerometer } from "expo-sensors";
+import { useAccelerometer } from "@use-expo/sensors";
 import { AppLoading } from "expo";
 import { Asset } from "expo-asset";
 
 const { width, height } = Dimensions.get("window");
 
+StatusBar.setHidden(true);
+
 export default () => {
-	const [tilt, setTilt] = React.useState(0);
+	const [{ y }] = useAccelerometer({ interval: 100, initial: { y: 0 } });
 	const [loading, setLoading] = React.useState(true);
-
-	React.useEffect(() => {
-		StatusBar.setHidden(true);
-		const _tilt = Accelerometer.addListener(({ y }) => setTilt(Math.round(y * 50) / 50));
-
-		return () => _tilt.remove();
-	}, []);
 
 	return loading
 		? (
@@ -27,7 +22,7 @@ export default () => {
 		: (
 			<View style={styles.container}>
 				<Image source={require("./assets/road.gif")} style={styles.road} />
-				<Image source={require("./assets/car.png")} style={[styles.car, { left: width * (0.48 + tilt) }]} />
+				<Image source={require("./assets/car.png")} style={[styles.car, { left: width * (0.48 + y) }]} />
 			</View>
 		);
 };
